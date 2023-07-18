dde847b9bad9b485fbb21aa32cd79b3480c3a3f0
Ventkatapathy <ashvanth.ventkatapathy@dnb.no>
2023-07-18 10:15:59 +0200

    First commit

---
+++ src/main/java/org/example/service/FootballScoreBoard.java
@@ -0,0 +1,42 @@
+package org.example.service;
+
+
+import org.example.domain.Match;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FootballScoreBoard {
+
+    private List<Match> matches;
+
+    public FootballScoreBoard() {
+        this.matches = new ArrayList<>();
+    }
+
+    public void startGame(String homeTeam, String awayTeam) {
+        Match match = new Match(homeTeam, awayTeam);
+        matches.add(match);
+    }
+
+    public void finishGame(String homeTeam, String awayTeam) {
+        matches.removeIf(match -> match.getHomeTeam().equals(homeTeam) && match.getAwayTeam().equals(awayTeam));
+    }
+
+    public void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore) {
+        matches.stream()
+                .filter(match -> match.getHomeTeam().equals(homeTeam) && match.getAwayTeam().equals(awayTeam))
+                .findFirst()
+                .ifPresent(match -> match.updateScore(homeScore, awayScore));
+    }
+
+
+    public List<Match> getSummaryByTotalScore() {
+        return matches.stream()
+                .sorted(Comparator.comparingInt(Match::getTotalScore).reversed().thenComparing(matches::indexOf))
+                .collect(Collectors.toList());
+    }
+
+}
